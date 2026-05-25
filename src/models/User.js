a2551b31a4bc8709ebeec0156632e452bd27327f
Jasmine Bedhammar <jasmine_bedhammar@hotmail.com>
2026-05-25 11:03:49 +0200

    Added email validation and profile pic

--- src/models/User.js
+++ src/models/User.js
@@ -5,12 +5,19 @@ const userSchema = new mongoose.Schema(
     username: {
       type: String,
       required: true,
+      unique: true,
     },
     email: {
       type: String,
       required: true,
+      unique: true,
+      match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, "Please enter a valid email address"],
     },
-    // TODO: Add profileImage field
+    profileImage: {
+      type: String,
+      required: false,
+      match: [/^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/, "Please enter a valid URL"],
+    },
   },
   { timestamps: true }
 );
